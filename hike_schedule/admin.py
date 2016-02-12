0bf8af8cb956b3029ef9c744e9a99e683cf4363c
Keith Dechant <keith.dechant@gmail.com>
2016-02-11 21:38:09 -0800

    Added Route and Area models

--- hike_schedule/admin.py
+++ hike_schedule/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import EventType, Event
+from .models import *
 
 @admin.register(EventType)
 class EventTypeAdmin(admin.ModelAdmin):
@@ -9,5 +9,14 @@ class EventTypeAdmin(admin.ModelAdmin):
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
     list_display = ['event_type', 'title', 'event_date', 'status']
-    list_filter = ['event_type']
-    
+    list_filter = ['event_type', 'route_id', 'leaders']
+
+@admin.register(Area)
+class AreaAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+@admin.register(Route)
+class RouteAdmin(admin.ModelAdmin):
+    list_display = ['name', 'distance', 'elevation_gain', 'favorite']
+    list_filter = ['area_id', 'favorite']
+
--- hike_schedule/models.py
+++ hike_schedule/models.py
@@ -1,10 +1,34 @@
-from django.db import models                                                                                                            
+from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
+class Area(models.Model):
+    name = models.CharField(max_length=200, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Route(models.Model):
+    name = models.CharField(max_length=200, blank=True, null=True)
+    description = models.TextField(default="", blank=True, null=True)
+    distance = models.FloatField(default=0, blank=True, null=True)
+    route_type = models.CharField(max_length=200, blank=True, null=True)
+    elevation_gain = models.IntegerField(default=0, blank=True, null=True)
+    drive_distance = models.IntegerField(default=0, blank=True, null=True)
+    favorite = models.BooleanField(default=0)
+    area_id = models.ForeignKey(
+        Area,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+
+    def __str__(self):
+        return self.name
+
 class EventType(models.Model):
     name = models.CharField(max_length=200, blank=True, null=True)
-    
+
     def __str__(self):
         return self.name
 
@@ -21,6 +45,12 @@ class Event(models.Model):
     drive_distance = models.IntegerField(default=0, blank=True, null=True)
     pace = models.CharField(max_length=200, blank=True, null=True)
     leaders = models.ManyToManyField(User)
+    route_id = models.ForeignKey(
+        Route,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
     status = models.CharField(max_length=200, blank=True, null=True,
         choices=[
             ('pending', 'Pending'),
@@ -28,7 +58,6 @@ class Event(models.Model):
             ('went', 'Went'),
             ('canceled', 'Canceled')
         ])
-    
+
     def __str__(self):
-        return self.event_type + ': ' + self.title
-    
+        return str(self.event_type) + ': ' + self.title
